05e506218ac039927d1c59eaf51cda609835a323
Κωνσταντίνος Μπατζιάκας <kmpatziakas@yahoo.gr>
2020-09-08 12:14:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if( ! isset($_SESSION['username'])) {
     <body>
     <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Εργαστηριο 3</a>
+  <a class="navbar-brand" href="#">eshop</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
